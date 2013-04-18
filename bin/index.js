0bd76e68fdb77fc7ca0b4838d75951902847b6f1
Giakki <martino.giacomo@gmail.com>
2013-04-18 22:25:42 +0200

    fixed version; added .gitignore

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,7 @@ var uncss   = require('../lib/uncss.js'),
 
 /* Parse command line options */
 program
-    .version('0.1.0')
+    .version('0.2.4-1')
     .usage('[options] <file.html file.css ...>')
     .option('-m, --minify', 'Minify css output')
     .option('-o, --outfile <file>', 'Redirect output to <file>')
