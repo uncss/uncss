5ee3781666b58a5cbe5f192243b4b3e5b16c7b9e
giakki <martino.giacomo@gmail.com>
2013-10-09 20:32:07 +0200

    minor fixes, added commander for cleaner code, preparing for phantomjs

--- bin/index.js
+++ bin/index.js
@@ -1,44 +1,17 @@
 #!/usr/bin/env node
 var uncss   = require('../lib/uncss.js'),
     utility = require('../lib/utility.js'),
+    program = require('commander'),
+    buffer  = '';
 
-    arg,
-    argv,
-    buffer = '',
-    callback,
-    compress,
-    files   = [],
-    i,
-    options = {};
+program
+    .version('0.4.4')
+    .usage('[options] <file ...>')
+    .option('-c, --compress', 'Compress CSS output')
+    .parse(process.argv);
 
-/* Parse command line options */
-argv = process.argv.splice(2);
-
-for (i = 0; i < argv.length; i++) {
-    arg = argv[i];
-    if (arg.length < 2) {
-        console.log('Unrecognized token: ' + arg);
-        process.exit();
-    }
-    if (arg[0] === '-') {
-        options[arg[1]] = true;
-    } else {
-        files.push(arg);
-    }
-}
-
-if (options.h) {
-    utility.showHelp();
-    process.exit();
-}
-
-if (options.c) {
-    options.compress = true;
-} else {
-    options.compress = false;
-}
-
-if (files.length === 0) {
+if (program.args.length === 0) {
+    // No files were specified, read html from stdin
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
 
@@ -47,12 +20,13 @@ if (files.length === 0) {
     });
 
     process.stdin.on('end', function() {
-        uncss(buffer, options, function (uncss) {
+        uncss(buffer, program.options, function (uncss) {
             console.log(uncss);
-        });        
+        });
     });
 } else {
-    uncss(files, options, function (uncss) {
+    uncss(program.args, program.options, function (uncss) {
         console.log(uncss);
-    });  
+    });
 }
+
