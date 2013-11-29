b5500f3cb81fd4c282f6caabeb3bc3e86d5b5a35
Giacomo Martino <giakki.dev@gmail.com>
2013-11-29 15:53:08 +0100

    Started over; PhantomJS working

---
+++ tests/js/script.js
@@ -0,0 +1,25 @@
+var container, row, column, heading, text;
+
+container = document.getElementById('add-here');
+
+row = document.createElement('div');
+row.className = 'row';
+
+column = document.createElement('div');
+column.className = 'col-lg-4';
+
+heading = document.createElement('h3');
+heading.className = 'green';
+heading.innerHTML = 'PhantomJS';
+
+text = document.createTextNode("PhantomJS works! This paragraph was added at runtime.");
+
+column.appendChild(heading);
+column.appendChild(text);
+row.appendChild(column);
+
+if (container.firstChild) {
+    container.insertBefore(row, container.firstChild);
+} else {
+    container.appendChild(row);
+}
