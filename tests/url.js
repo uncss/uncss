a0b3712ce704dab61e1e1fbf799e1de27ec79cb0
Xavier Damman <xdamman@gmail.com>
2014-01-01 22:22:17 -0800

    Moved output to tests/

--- tests/url.js
+++ tests/url.js
@@ -8,7 +8,7 @@ describe("Compile the CSS of an html page passed by url", function() {
     this.timeout(10000);
     uncss(["http://getbootstrap.com/examples/jumbotron/"], function(output) {
       expect(output).to.exist;
-      fs.writeFile("output/jumbotron.compiled.css", output, done);
+      fs.writeFile(__dirname+"/output/jumbotron.compiled.css", output, done);
     });
   });
 
@@ -16,7 +16,7 @@ describe("Compile the CSS of an html page passed by url", function() {
     this.timeout(10000);
     uncss(["http://thenextweb.com/insider/2013/11/06/storify-integrates-twitter-turn-story-slideshow-embed-tweet/"], function(output) {
       expect(output).to.exist;
-      fs.writeFile("output/thenextweb.compiled.css", output, done);
+      fs.writeFile(__dirname+"/output/thenextweb.compiled.css", output, done);
   
     });
   });
