7ea90e82440f49b97e725a8bf109f967fe741716
Giacomo Martino <giakki.dev@gmail.com>
2014-01-02 16:23:41 +0100

    Tests now run against gh-pages; Fix for http-prefixed stylesheets

--- tests/url.js
+++ tests/url.js
@@ -8,15 +8,15 @@ describe('Compile the CSS of an html page passed by url', function () {
         this.timeout(10000);
         uncss(['http://getbootstrap.com/examples/jumbotron/'], function (output) {
             expect(output).to.exist;
-            fs.writeFile(__dirname + '/output/jumbotron.compiled.css', output, done);
+            fs.writeFile(__dirname + '/output/bootstrap/jumbotron.compiled.css', output, done);
         });
     });
 
     it('Deal with CSS files linked with absolute url', function (done) {
         this.timeout(10000);
-        uncss(['http://thenextweb.com/insider/2013/11/06/storify-integrates-twitter-turn-story-slideshow-embed-tweet/'], function (output) {
+        uncss(['http://giakki.github.io/uncss/'], function (output) {
                 expect(output).to.exist;
-                fs.writeFile(__dirname + '/output/thenextweb.compiled.css', output, done);
+                fs.writeFile(__dirname + '/output/gh-pages/stylesheets/stylesheet.css', output, done);
         });
     });
 
