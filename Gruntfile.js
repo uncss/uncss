7b78d2c89e18df9ffb53988568c1ae964c7e4ec3
XhmikosR <xhmikosr@users.sourceforge.net>
2014-01-25 10:59:59 +0200

    Switch to Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            bin: {
+                src: ['bin/uncss']
+            },
+            lib: {
+                src: ['lib/**/*.js']
+            },
+            tests: {
+                options: {
+                    globals: {
+                        describe: true,
+                        it: true
+                    }
+                },
+                src: ['tests/*.js']
+            },
+        },
+
+        mochacov: {
+            options: {
+                reporter: 'spec'
+            },
+            all: ['tests/*.js']
+        },
+
+
+    });
+
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+    require('time-grunt')(grunt);
+
+    grunt.registerTask('test', ['jshint', 'mochacov']);
+    grunt.registerTask('default', ['jshint']);
+
+};
