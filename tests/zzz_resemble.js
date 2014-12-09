b1a599e6ee8949e45d757b3affb4d44307529b47
Giacomo Martino <martino.giacomo@gmail.com>
2014-12-09 21:40:16 +0100

    Fix #39 - Tests failing on Windows

--- tests/zzz_resemble.js
+++ tests/zzz_resemble.js
@@ -3,30 +3,25 @@
 
 var chai     = require('chai'),
     expect   = chai.expect,
-    path     = require('path'),
     resemble = require('chai-resemble');
 
 chai.use(resemble);
-
-function abs(file) {
-    return path.join(__dirname, file);
-}
 
 describe('Pages should resemble the reference', function () {
     this.timeout(15000);
 
     it('Bootstrap', function (done) {
-        expect(abs('output/bootstrap/jumbotron.html'))
+        expect('tests/output/bootstrap/jumbotron.html')
             .to.resemble('http://getbootstrap.com/examples/jumbotron/', done);
     });
 
     it('Github pages', function (done) {
-        expect(abs('output/gh-pages/index.html'))
+        expect('tests/output/gh-pages/index.html')
             .to.resemble('http://giakki.github.io/uncss/', done);
     });
 
     it('Selectors', function (done) {
-        expect(abs('selectors/index.html'))
-            .to.resemble(abs('output/selectors/index.html'), done);
+        expect('tests/selectors/index.html')
+            .to.resemble('tests/output/selectors/index.html', done);
     });
 });
