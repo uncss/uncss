a0a02b4ac64e3c4d26accc3ddb07708b0420853c
RyanZim <opensrc@ryanzim.com>
2017-10-07 15:03:24 -0400

    Use ES6 syntax supported by Node 6+

--- tests/errors.js
+++ tests/errors.js
@@ -1,82 +1,82 @@
 'use strict';
 
-var expect = require('chai').expect,
+const expect = require('chai').expect,
     uncss = require('./../src/uncss.js');
 
-var invalidCss = 'We need to create a string longer than 40 characters to ' +
+const invalidCss = 'We need to create a string longer than 40 characters to ' +
                  'check if the error string we are creating is helpful';
 
-describe('Error reporting', function () {
+describe('Error reporting', () => {
 
-    it('No callback', function () {
+    it('No callback', () => {
         function throwTest () {
             uncss('<html></html>', { stylesheets: ['nonexistent'] });
         }
         expect(throwTest).to.throw(TypeError);
     });
 
-    it('No valid HTML files', function (done) {
-        uncss(['nonexistent.html'], function (error, output) {
+    it('No valid HTML files', (done) => {
+        uncss(['nonexistent.html'], (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.equal('UnCSS: no HTML files found');
             done();
         });
     });
 
-    it('Invalid options.stylesheets', function (done) {
+    it('Invalid options.stylesheets', (done) => {
         uncss('<html></html>', {
             stylesheets: ['nonexistent']
-        }, function (error, output) {
+        }, (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.contain('UnCSS: could not open');
             done();
         });
     });
 
-    it('Invalid options.stylesheets with URL', function (done) {
+    it('Invalid options.stylesheets with URL', (done) => {
         uncss('<html></html>', {
             stylesheets: ['http://invalid']
-        }, function (error, output) {
+        }, (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.contain('ENOTFOUND');
             done();
         });
     });
 
-    it('Invalid options.raw', function (done) {
+    it('Invalid options.raw', (done) => {
         uncss('<html></html>', {
             raw: ['.test { margin: 0 }']
-        }, function (error, output) {
+        }, (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.equal('UnCSS: options.raw - expected a string');
             done();
         });
     });
 
-    it('No stylesheet found', function (done) {
-        uncss('<html><body></body></html>', function (error, output) {
+    it('No stylesheet found', (done) => {
+        uncss('<html><body></body></html>', (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.equal('UnCSS: no stylesheets found');
             done();
         });
     });
 
-    it('jsdom errors', function (done) {
-        uncss(['http://invalid'], function (error, output) {
+    it('jsdom errors', (done) => {
+        uncss(['http://invalid'], (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.contain('getaddrinfo ENOTFOUND invalid invalid:80');
             done();
         });
     });
 
-    it('jsdom errors to stderr', function (done) {
-        var stderrBuffer = '';
-        var oldWrite = process.stderr.write;
+    it('jsdom errors to stderr', (done) => {
+        let stderrBuffer = '';
+        const oldWrite = process.stderr.write;
         process.stderr.write = function (data) {
             stderrBuffer += data;
         };
 
-        uncss(['tests/jsdom/throw.html'], { raw: '' }, function (error) {
+        uncss(['tests/jsdom/throw.html'], { raw: '' }, (error) => {
             expect(error).to.equal(null);
             expect(stderrBuffer).to.contain('Exception');
             process.stderr.write = oldWrite;
@@ -84,46 +84,46 @@ describe('Error reporting', function () {
         });
     });
 
-    it('css-parse errors', function (done) {
+    it('css-parse errors', (done) => {
         uncss(['tests/selectors/index.html'], {
             raw: invalidCss
-        }, function (error, output) {
+        }, (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.contain('unable to parse');
             done();
         });
     });
 
-    it('css-parse errors (minified stylesheet)', function (done) {
+    it('css-parse errors (minified stylesheet)', (done) => {
         uncss(['tests/selectors/index.html'], {
             stylesheets: ['../coverage/minified.css']
-        }, function (error, output) {
+        }, (error, output) => {
             expect(output).to.equal(undefined);
             expect(error.message).to.contain('unable to parse');
             done();
         });
     });
 
-    it('Report should be generated only if specified', function (done) {
-        uncss(['tests/selectors/index.html'], function (error, output, report) {
+    it('Report should be generated only if specified', (done) => {
+        uncss(['tests/selectors/index.html'], (error, output, report) => {
             expect(report).to.equal(undefined);
             done();
         });
     });
 
-    it('Reports when the uncssrc file does not exist', function (done) {
+    it('Reports when the uncssrc file does not exist', (done) => {
         uncss(['selectors/index.html'], {
             uncssrc: 'nonexistent'
-        }, function (err) {
+        }, (err) => {
             expect(err.code).to.equal('ENOENT');
             done();
         });
     });
 
-    it('Reports errors in the uncssrc file', function (done) {
+    it('Reports errors in the uncssrc file', (done) => {
         uncss(['selectors/index.html'], {
             uncssrc: 'tests/coverage/.invaliduncssrc'
-        }, function (err) {
+        }, (err) => {
             expect(err).to.be.an.instanceOf(SyntaxError);
             expect(err.message).to.equal('UnCSS: uncssrc file is invalid JSON.');
             done();
