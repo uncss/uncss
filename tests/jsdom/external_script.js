9e0823efd297b9593fafee70e3c7f15dddf976bb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-25 21:41:12 +0800

    Delete tests directory

--- tests/jsdom/external_script.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env browser */
-'use strict';
-
-let div = document.createElement('div');
-div.className = 'evaluated';
-document.body.appendChild(div);
--- tests/jsdom/inject.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function(window) {
-    window.document.querySelector('html').classList.add('no-test', 'test');
-};
